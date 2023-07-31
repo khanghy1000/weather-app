68c37a22c2fd7df48996d91edf6398cceec5435d
khanghy1000 <khanghy130603@gmail.com>
2023-07-31 22:03:31 +0700

    Display API data on UI

--- script.js
+++ script.js
@@ -13,3 +13,36 @@ const appController = () => {
 
 	return { getWeather };
 };
+
+const screenController = () => {
+	const app = appController();
+
+	const form = document.querySelector('form');
+	const searchBar = document.querySelector('[data-search-bar]');
+	const condition = document.querySelector('[data-condition]');
+	const location = document.querySelector('[data-location]');
+	const temp = document.querySelector('[data-temp]');
+	const fellLike = document.querySelector('[data-fell-like]');
+	const wind = document.querySelector('[data-wind]');
+	const humidity = document.querySelector('[data-humidity]');
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const location = searchBar.value;
+		updateScreen(location);
+	});
+
+	async function updateScreen(input) {
+		let weather = await app.getWeather(input);
+		condition.textContent = weather.current.condition.text;
+		location.textContent = `${weather.location.name}, ${weather.location.country}`;
+		temp.textContent = weather.current.temp_c;
+		fellLike.textContent = `Feels like: ${weather.current.feelslike_c}`;
+		wind.textContent = `Wind: ${weather.current.wind_mph} mph`;
+		humidity.textContent = `Humidity: ${weather.current.humidity}%`;
+	}
+
+	updateScreen('Vietnam');
+};
+
+screenController();
